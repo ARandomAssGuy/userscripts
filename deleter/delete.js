c769e538d7214cc50dd3e431e1541501bf37c9b4
ARandomAssGuy <68760804+ARandomAssGuy@users.noreply.github.com>
2020-09-20 20:07:28 +0200

    Update delete.js

--- deleter/delete.js
+++ deleter/delete.js
@@ -4,10 +4,10 @@ Java would be a better language to code this in, but idk how 2 java (yet).
 i do not take any responsibility for how this script is used.
 */
 
-
 const fs = require('fs');
 const crypto = require('crypto');
-
+const readline = require('readline');
+directory = false;
 file = process.argv[2] || undefined;
 
 if (file==undefined) {
@@ -18,18 +18,55 @@ if (file==undefined) {
   console.error('No such file was found.');
   process.exit(0);
 };
-i=0;
-filesize = fs.statSync(file).size;
+
+amount = process.argv[3] || 1000;
+
 function overWriteFile(file, filesize) {
   i++;
   rbytes = crypto.randomBytes(filesize+1024);
   fs.writeFileSync(file, rbytes);
   console.log('Overwrite number '+i+' completed.');
 };
+function getFileSizes(files, dir) {
+  let filesizes={};
+  files.forEach((file)=>{
+    filesizes[file]=fs.statSync(dir+'/'+file).size;
+  });
+  return filesizes;
+};
 
-amount = process.argv[3] || 1000
+function overWriteDir(sizes, files, filename) {
+  i++;
+  files.forEach((file)=>{
+    let rbytes = crypto.randomBytes(sizes[file]);
+    fs.writeFileSync(filename+'/'+file, rbytes);
+    console.log('Overwrite number '+i+' for file '+file+' completed.');
+  });
+};
 
-overWriter = setInterval(()=>{
-if (i>amount) {clearInterval(overWriter); console.log('Completed overwrite.'); process.exit(0);};
-overWriteFile(file, filesize);
-}, 1);
+i=0;
+fileinfo = fs.statSync(file);
+filesize = fileinfo.size;
+
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
+
+if (fileinfo.isDirectory()) {
+  directory = true;
+  console.log('!WARNING! THIS FILE IS A DIRECTORY !WARNING!');
+  console.log('!WARNING! ARE  YOU SURE YOU WOULD LIKE TO DELETE ALL FILES IN THIS DIRECTORY? !WARNING!');
+  rl.question('!WARNING! IF YES, THEN PLEASE TYPE '+file+' !WARNING!\n', (confirmation) => {
+    if (confirmation == file) {
+      let files = fs.readdirSync(file);
+      let sizes = getFileSizes(files, file);
+      overWriter = setInterval(()=>{
+        if (i>amount) {clearInterval(overWriter); console.log('Completed overwrite.'); process.exit(0);};
+        overWriteDir(sizes, files, file);
+      }, 1);
+    };
+  });
+} else {
+  overWriter = setInterval(()=>{
+    if (i>amount) {clearInterval(overWriter); console.log('Completed overwrite.'); process.exit(0);};
+    overWriteFile(file, filesize);
+  }, 1);
+};
