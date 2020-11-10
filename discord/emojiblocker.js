8b0b7330b92363202b303a0cffe7d014d3011d3e
ARandomAssGuy <68760804+ARandomAssGuy@users.noreply.github.com>
2020-11-10 08:37:10 +0100

    added *= for INCLUDES instead so you can change value easier.

--- discord/emojiblocker.js
+++ discord/emojiblocker.js
@@ -13,7 +13,7 @@ you can update the array at any time and the changes are seen immediately
 illegalemojis=["/assets/81a553e7c02c5dd144e40888c4b2faad.svg"]
 setInterval(()=>{
 illegalemojis.forEach((emoji)=>{
-    let lol=document.querySelector('img[src="'+emoji+'"]')
+    let lol=document.querySelector('img[src*="'+emoji+'"]') 
     if (lol) {
       lol.remove()
     }
